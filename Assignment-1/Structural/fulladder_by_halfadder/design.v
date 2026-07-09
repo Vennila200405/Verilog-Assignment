1bd1ad51baa8d76b19c09f4cfd3154cdb313a3fa
Vennila200405 <vennilas2004@gmail.com>
2026-07-09 20:30:59 +0530

    Create design.v

---
+++ Assignment-1/Structural/fulladder_by_halfadder/design.v
@@ -0,0 +1,41 @@
+module Half_Adder(
+    input a,
+    input b,
+    output sum,
+    output carry
+);
+assign sum = a ^ b;
+assign carry = a & b;
+endmodule
+
+//Fulladder
+
+module Full_Adder(
+    input a,
+    input b,
+    input cin,
+    output sum,
+    output reg cout
+);
+
+wire s1;
+wire c1;
+wire c2;
+
+Half_Adder HA1(
+    .a(a),
+    .b(b),
+    .sum(s1),
+    .carry(c1)
+);
+
+Half_Adder HA2(
+    .a(s1),
+    .b(cin),
+    .sum(sum),
+    .carry(c2)
+);
+always @(*) begin
+    cout = c1 | c2;
+end
+endmodule
